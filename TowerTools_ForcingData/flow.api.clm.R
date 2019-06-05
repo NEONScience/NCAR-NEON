8ccd2914e0d906a9e9a5f39fe28f2f3262668b0a
ddurden <daviddurden27@gmail.com>
2019-06-05 19:53:19 +0000

    - API workflow for grabbing forcing data

---
+++ TowerTools_ForcingData/flow.api.clm.R
@@ -0,0 +1,41 @@
+#Call the R HDF5 Library
+packReq <- c("rhdf5", "eddy4R.base", "neonUtilities")
+
+#Install and load all required packages
+lapply(packReq, function(x) {
+  print(x)
+  if(require(x, character.only = TRUE) == FALSE) {
+    install.packages(x)
+    library(x, character.only = TRUE)
+  }})
+
+#############################################################
+#Workflow parameters
+#############################################################
+#Which NEON site are we grabbing data from (4-letter ID)
+Site <- "HARV"
+#Which type of data package (expanded or basic)
+Pack <- "basic"
+#Time averaging period
+TimeAgr <- 30
+#Beginning date for data grabbing
+dateBgn <- "2018-06-01"
+
+#End date for date grabbing
+dateEnd <- "2018-07-01"
+
+
+#List of DP numbers by eddy4R DP names
+listDpNum <- c("TBOT" = "DP1.00003.001", "PRECTmms" = "DP1.00006.001", "WIND" = "DP1.00001.001", "PSRF" = "DP1.00004.001", "RH" = "DP1.00098.001", "FLDS" = "DP1.00023.001", "FSDS" = "DP1.00023.001")
+
+#Year and month of interest
+
+#varMetaData <- c("lat" = "LatTow", "lon" = "LonTow", "ZBOT" = "DistZaxsTow" )
+
+#varReqData <- c("TBOT" = "tempAir", "FLDS" =, "FSDS" =, "PRECTmms" = , "PSRF", "RH")
+
+#Grab data for data products using neonUtilities
+#neonUtilities::getPackage(site_code = site, package = pack, year_month =  )
+
+##Grab data for data products using Noble package
+data <- try(expr = Noble::pull.date(site = Site, dpID = listDpNum[[1]], bgn.date = dateBgn, end.date = dateEnd, package = "basic", time.agr = TimeAgr), silent = TRUE)
