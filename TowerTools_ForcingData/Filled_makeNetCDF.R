71d8ab56a72aba3f4dfbb3209c2afd08c14e79c3
wwieder <wwieder@ucar.edu>
2019-06-19 10:58:07 -0600

    update code and functions to ncdf4 package

--- TowerTools_ForcingData/Filled_makeNetCDF.R
+++ TowerTools_ForcingData/Filled_makeNetCDF.R
@@ -10,9 +10,10 @@
 # All other data from Tvan towers, from John Knowles 2015
 
   library(REddyProc)
-  library(ncdf)
+  library(ncdf4)
 
   dir = "~/Desktop/Working_files/Niwot/NR_fluxes/TVan/mpg_formatted/out_2008_2013"
+  outdir = "~/Desktop/temp/"
   setwd(dir)
   file <- paste(dir,"Filled_makeNetCDF.R", sep = "")
 
@@ -44,8 +45,8 @@ LON <- 254.4142
 #---------------write out .nc file----------------------------
 #-------------------------------------------------------------
 # define the netcdf coordinate variables (name, units, type)
-lat     <- dim.def.ncdf("lat","degrees_north", as.double(LAT), create_dimvar=TRUE)
-lon     <- dim.def.ncdf("lon","degrees_east", as.double(LON), create_dimvar=TRUE)
+lat     <- ncdim_def("lat","degrees_north", as.double(LAT), create_dimvar=TRUE)
+lon     <- ncdim_def("lon","degrees_east", as.double(LON), create_dimvar=TRUE)
 
 # Make varables (name, units, dims, missing_value)
   mv <- -9999.             # missing value to use
@@ -66,57 +67,57 @@ lon     <- dim.def.ncdf("lon","degrees_east", as.double(LON), create_dimvar=TRUE
       Data.mon <- Data.F[startStep:endStep, ]
       print(paste(year[y],m,"Data date =",Data.mon$DateTime[1]))
       names(Data.mon)
-      time <- dim.def.ncdf("time", units=paste("days since",Data.mon$DateTime[1],"00:00:00"),
-                           vals=as.double(time),unlim=FALSE, create_dimvar=TRUE )
-      LATIXY  <- var.def.ncdf("LATIXY", "degrees N", list(lat), mv,
-                              longname="latitude", prec="double")
-      LONGXY  <- var.def.ncdf("LONGXY", "degrees E", list(lon), mv,
-                              longname="longitude", prec="double")
-      FLDS  <- var.def.ncdf("FLDS", "W/m^2", list(lon,lat,time), mv,
-                            longname="incident longwave (FLDS)", prec="double")
-      FSDS  <- var.def.ncdf("FSDS", "W/m^2", list(lon,lat,time), mv,
-                            longname="incident shortwave (FSDS)", prec="double")
-      PRECTmms <- var.def.ncdf("PRECTmms", "mm/s", list(lon,lat,time), mv,
-                               longname="precipitation (PRECTmms)", prec="double")
-      PSRF  <- var.def.ncdf("PSRF", "Pa", list(lon,lat,time), mv,
-                            longname="pressure at the lowest atmospheric level (PSRF)", prec="double")
-      RH    <- var.def.ncdf("RH", "%", list(lon,lat,time), mv,
-                            longname="relative humidity at lowest atm level (RH)", prec="double")
-      TBOT  <- var.def.ncdf("TBOT", "K", list(lon,lat,time), mv,
-                            longname="temperature at lowest atm level (TBOT)", prec="double")
-      WIND  <- var.def.ncdf("WIND", "m/s", list(lon,lat,time), mv,
-                            longname="wind at lowest atm level (WIND)", prec="double")
-      ZBOT  <- var.def.ncdf("ZBOT", "m", list(lon,lat,time), mv,
-                            longname="observational height", prec="double")
+      time <- ncdim_def("time", units=paste("days since",Data.mon$DateTime[1],"00:00:00"),
+                           vals=as.double(time$vals),unlim=FALSE, create_dimvar=TRUE )
+      LATIXY  <- ncvar_def("LATIXY", "degrees N", dim = list(lat), mv,
+                              longname="latitude")
+      LONGXY  <- ncvar_def("LONGXY", "degrees E", list(lon), mv,
+                              longname="longitude")
+      FLDS  <- ncvar_def("FLDS", "W/m^2", list(lon,lat,time), mv,
+                            longname="incident longwave (FLDS)")
+      FSDS  <- ncvar_def("FSDS", "W/m^2", list(lon,lat,time), mv,
+                            longname="incident shortwave (FSDS)")
+      PRECTmms <- ncvar_def("PRECTmms", "mm/s", list(lon,lat,time), mv,
+                               longname="precipitation (PRECTmms)")
+      PSRF  <- ncvar_def("PSRF", "Pa", list(lon,lat,time), mv,
+                            longname="pressure at the lowest atmospheric level (PSRF)")
+      RH    <- ncvar_def("RH", "%", list(lon,lat,time), mv,
+                            longname="relative humidity at lowest atm level (RH)")
+      TBOT  <- ncvar_def("TBOT", "K", list(lon,lat,time), mv,
+                            longname="temperature at lowest atm level (TBOT)")
+      WIND  <- ncvar_def("WIND", "m/s", list(lon,lat,time), mv,
+                            longname="wind at lowest atm level (WIND)")
+      ZBOT  <- ncvar_def("ZBOT", "m", list(lon,lat,time), mv,
+                            longname="observational height")
       # Create a netCDF file with output variables
-      fname <- paste(year[y],"-",mon[m],".nc", sep = "")
-      ncnew <- create.ncdf( fname, list(LATIXY,LONGXY,FLDS,FSDS,PRECTmms,RH,PSRF,TBOT,WIND,ZBOT) )
+      fname <- paste(outdir, year[y],"-",mon[m],".nc", sep = "")
+      ncnew <- nc_create( fname, list(LATIXY,LONGXY,FLDS,FSDS,PRECTmms,RH,PSRF,TBOT,WIND,ZBOT) )
       # Write some values to this variable on disk.
-      put.var.ncdf( ncnew, LATIXY, LAT)
-      put.var.ncdf( ncnew, LONGXY, LON)
-      put.var.ncdf( ncnew, FLDS, Data.mon$FLDS)
-      put.var.ncdf( ncnew, FSDS, Data.mon$FSDS)
-      put.var.ncdf( ncnew, RH,   Data.mon$RH)
-      put.var.ncdf( ncnew, PRECTmms, Data.mon$PRECTmms)
-      put.var.ncdf( ncnew, PSRF, Data.mon$PSRF)
-      put.var.ncdf( ncnew, TBOT, Data.mon$TBOT)
-      put.var.ncdf( ncnew, WIND, Data.mon$WIND)
-      put.var.ncdf( ncnew, ZBOT, Data.mon$ZBOT)
+      ncvar_put( ncnew, LATIXY, LAT)
+      ncvar_put( ncnew, LONGXY, LON)
+      ncvar_put( ncnew, FLDS, Data.mon$FLDS)
+      ncvar_put( ncnew, FSDS, Data.mon$FSDS)
+      ncvar_put( ncnew, RH,   Data.mon$RH)
+      ncvar_put( ncnew, PRECTmms, Data.mon$PRECTmms)
+      ncvar_put( ncnew, PSRF, Data.mon$PSRF)
+      ncvar_put( ncnew, TBOT, Data.mon$TBOT)
+      ncvar_put( ncnew, WIND, Data.mon$WIND)
+      ncvar_put( ncnew, ZBOT, Data.mon$ZBOT)
       #add attributes
-      att.put.ncdf( ncnew, time,"calendar", "gregorian" ,prec=NA,verbose=FALSE,definemode=FALSE )
-      att.put.ncdf( ncnew, FLDS,"mode","time-dependent" ,prec=NA,verbose=FALSE,definemode=FALSE )
-      att.put.ncdf( ncnew, FSDS,"mode","time-dependent" ,prec=NA,verbose=FALSE,definemode=FALSE )
-      att.put.ncdf( ncnew, RH  ,"mode","time-dependent" ,prec=NA,verbose=FALSE,definemode=FALSE )
-      att.put.ncdf( ncnew, PRECTmms,"mode","time-dependent" ,prec=NA,verbose=FALSE,definemode=FALSE )
-      att.put.ncdf( ncnew, PSRF,"mode","time-dependent" ,prec=NA,verbose=FALSE,definemode=FALSE )
-      att.put.ncdf( ncnew, TBOT,"mode","time-dependent" ,prec=NA,verbose=FALSE,definemode=FALSE )
-      att.put.ncdf( ncnew, WIND,"mode","time-dependent" ,prec=NA,verbose=FALSE,definemode=FALSE )
-      att.put.ncdf( ncnew, ZBOT,"mode","time-dependent" ,prec=NA,verbose=FALSE,definemode=FALSE )
-      att.put.ncdf( ncnew, 0, "created_on",date()       ,prec=NA,verbose=FALSE,definemode=FALSE )
-      att.put.ncdf( ncnew, 0, "created_by","Will Wieder",prec=NA,verbose=FALSE,definemode=FALSE )
-      att.put.ncdf( ncnew, 0, "created_from",fin        ,prec=NA,verbose=FALSE,definemode=FALSE )
-      att.put.ncdf( ncnew, 0, "created_with",file       ,prec=NA,verbose=FALSE,definemode=FALSE )
-      close.ncdf(ncnew)
+      ncatt_put( ncnew, 'time', attname = "calendar", attval = "gregorian" ,prec=NA,verbose=FALSE,definemode=FALSE )
+      ncatt_put( ncnew, 'FLDS',"mode","time-dependent" ,prec=NA,verbose=FALSE,definemode=FALSE )
+      ncatt_put( ncnew, 'FSDS',"mode","time-dependent" ,prec=NA,verbose=FALSE,definemode=FALSE )
+      ncatt_put( ncnew, 'RH'  ,"mode","time-dependent" ,prec=NA,verbose=FALSE,definemode=FALSE )
+      ncatt_put( ncnew, 'PRECTmms',"mode","time-dependent" ,prec=NA,verbose=FALSE,definemode=FALSE )
+      ncatt_put( ncnew, 'PSRF',"mode","time-dependent" ,prec=NA,verbose=FALSE,definemode=FALSE )
+      ncatt_put( ncnew, 'TBOT',"mode","time-dependent" ,prec=NA,verbose=FALSE,definemode=FALSE )
+      ncatt_put( ncnew, 'WIND',"mode","time-dependent" ,prec=NA,verbose=FALSE,definemode=FALSE )
+      ncatt_put( ncnew, 'ZBOT',"mode","time-dependent" ,prec=NA,verbose=FALSE,definemode=FALSE )
+      ncatt_put( ncnew, 0, "created_on",date()       ,prec=NA,verbose=FALSE,definemode=FALSE )
+      ncatt_put( ncnew, 0, "created_by","Will Wieder",prec=NA,verbose=FALSE,definemode=FALSE )
+      ncatt_put( ncnew, 0, "created_from",fin        ,prec=NA,verbose=FALSE,definemode=FALSE )
+      ncatt_put( ncnew, 0, "created_with",file       ,prec=NA,verbose=FALSE,definemode=FALSE )
+      nc_close(ncnew)
       startStep <- endStep + 1
       remove(endStep, time, timeStep, fname, ncnew, Data.mon,
              FLDS,FSDS,RH,PRECTmms,PSRF,TBOT,WIND,ZBOT)
