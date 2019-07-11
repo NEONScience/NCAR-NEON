18413f63f8fea4b2cd5cc874a2cea084a85906c4
ddurden <daviddurden27@gmail.com>
2019-07-11 13:43:15 +0000

    - Adding date sequence

--- TowerTools_ForcingData/flow.api.clm.R
+++ TowerTools_ForcingData/flow.api.clm.R
@@ -1,5 +1,5 @@
 #Call the R HDF5 Library
-packReq <- c("rhdf5", "eddy4R.base", "neonUtilities")
+packReq <- c("rhdf5", "eddy4R.base", "neonUtilities", "REddyProc")
 
 #Install and load all required packages
 lapply(packReq, function(x) {
@@ -24,6 +24,24 @@ dateBgn <- "2018-06-01"
 #End date for date grabbing
 dateEnd <- "2018-07-01"
 
+
+#The version data for the FP standard conversion processing
+ver = paste0("v",format(Sys.time(), "%Y%m%dT%H%m"))
+#Download directory for HDF5 files from the API
+DirDnld=tempdir()
+#paste0("~/eddy/data/Ameriflux/",ver,"/",site)
+#Output directory
+if("DIROUT" %in% base::names(base::Sys.getenv())) {
+  DirOutBase <- Sys.getenv("DIROUT")
+}else{  
+  #DirOut <- "N:/Science/FIUDATA/IPT_data/dynamic/AmeriFlux/" #Default folder Ameriflux data output
+  DirOutBase <-paste0("~/eddy/data/CLM/",ver)
+  
+}
+
+
+
+
 #Define site info
 siteInfo <- Noble::tis_site_config
 siteInfo <- siteInfo[siteInfo$SiteID == Site,]
@@ -36,6 +54,24 @@ lon <- siteInfo$Longitude
 listDpNum <- c("TBOT" = "DP1.00003.001", "PRECTmms" = "DP1.00006.001", "WIND" = "DP1.00001.001", "PSRF" = "DP1.00004.001", "RH" = "DP1.00098.001", "FLDS" = "DP1.00023.001", "FSDS" = "DP1.00023.001")
 
 varDp <- c("TBOT" = "tempTripleMean", "PRECTmms" = "secPrecipBulk", "WIND" = "windSpeedMean.000.050", "PSRF" = "staPresMean", "RH" = "RHMean", "FLDS" = "inLWMean", "FSDS" = "inSWMean")
+
+
+
+
+#Create the method for date sequence based off of the package chosen
+MethDateSeq <- ifelse(test = Pack == "basic", yes = "month", no = "day")
+
+#If monthly files from basic chosen round to the beginning and end of the month for dateBgn and dateEnd
+if(MethDateSeq == "month"){
+  dateBgn <- lubridate::floor_date(as.Date(dateBgn), unit = "month")
+  dateEnd <- lubridate::ceiling_date(as.Date(dateEnd), unit = "month") - lubridate::days(1)
+}
+#Create the date sequence
+setDate <- seq(from = as.Date(dateBgn), to = as.Date(dateEnd), by = MethDateSeq)
+
+
+
+
 #Year and month of interest
 
 #varMetaData <- c("lat" = "LatTow", "lon" = "LonTow", "ZBOT" = "DistZaxsTow" )
@@ -50,4 +86,21 @@ data <- lapply(listDpNum, function(x){
   try(expr = Noble::pull.date(site = Site, dpID = x, bgn.date = dateBgn, end.date = dateEnd, package = Pack, time.agr = TimeAgr), silent = TRUE)
   })
 
+#ReddyProc workflow
+EddyProc.C <- sEddyProc$new('NR-TVan', EddyDataWithPosix.F, c('NEE','Rg','Tair','VPD','ws_MDS','Rn_MDS','Rh','LE','H','Pa_MDS'))
+#+++ Generate plots of all data in directory \plots (of current R working dir)
+EddyProc.C$sPlotHHFluxes('NEE')
+EddyProc.C$sPlotFingerprint('Rg')
+EddyProc.C$sPlotDiurnalCycle('Tair')
+EddyProc.C$sPlotDiurnalCycle('NEE')
+#+++ Plot individual months/years to screen (of current R graphics device)
+years <- seq(2008,2014,1)
+nyears <- length(years)
+for (i in 1:nyears) {  
+  EddyProc.C$sPlotHHFluxesY('NEE', Year.i=years[i])
+  EddyProc.C$sPlotFingerprintY('NEE', Year.i=years[i])
+}
+EddyProc.C$sPlotDiurnalCycleM('NEE', Month.i=6)
 
+#+++ Fill gaps in variables with MDS gap filling algorithm (without prior ustar filtering)
+EddyProc.C$sMDSGapFill('NEE', FillAll.b=TRUE) #Fill all values to estimate flux uncertainties
