d2619c66a22f7afd92a3d995a5a61986f582fbbc
ddurden <daviddurden27@gmail.com>
2020-06-08 03:00:33 +0000

    - testing build hook

--- hooks/post_push.sh
+++ hooks/post_push.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
+set -e
 
 # $IMAGE_NAME var is injected into the build so the tag is correct. 
 echo "Build hook running"
+
+echo "tagging $IMAGE_NAME as $DOCKER_REPO:$VERSION..."
+
+
 docker build . --build-arg VCS_REF=`git rev-parse — short HEAD` \
   --build-arg BUILD_DATE=`date -u +”%Y-%m-%dT%H:%M:%SZ”` \
   -t $IMAGE_NAME 
