3ac9ec44af04232770442caec6890b38ded1676b
ddurden <daviddurden27@gmail.com>
2020-10-30 16:20:13 +0000

    - Adding workflow to analyze driver variables the data for gaps

---
+++ TowerTools_ForcingData/flow.data.gap.R
@@ -0,0 +1,144 @@
+library(dplyr)
+library(tidyr)
+
+Site <- "NIWO"
+
+dateBgn <- "2018-01-01"
+dateEnd <- "2019-01-01"
+TimeAgr <- "30"
+Pack <- "basic"
+
+#List of DP numbers by eddy4R DP names
+listDpNum <- c( "PRECTmms_MDS" = "DP1.00006.001", "rH" = "DP1.00098.001", "FLDS_MDS" = "DP1.00023.001", "Pa_MDS" = "DP1.00004.001", "TempTop" = "DP1.00003.001")
+
+#names for individual variables of interest
+varDp <- c("PRECTmms_MDS" = "SECPRE_30min", "rH" = "RH_30min", "FLDS_MDS" = "SLRNR_30min", "Rg" = "SLRNR_30min", "Pa_MDS" = "BP_30min") #Currently using the relative humidity from the soil array, tower top was not reporting data at HARV during this time
+
+subVar <- c("PRECTmms_MDS" = "secPrecipBulk", "rH" = "RHMean", "FLDS_MDS" = "inLWMean", "Rg" = "inSWMean", "Pa_MDS" = "staPresMean")
+
+subVarQf <- c("PRECTmms_MDS" = "secPrecipFinalQF", "rH" = "RHFinalQF", "FLDS_MDS" = "inLWFinalQF", "Rg" = "inSWFinalQF", "Pa_MDS" = "staPresFinalQF")
+
+#test <- Noble::gap.vis(site = "NIWO",bgn.month = "2018-01", end.month = "2020-01", dpID = "DP1.00003.001", save.dir = "~/eddy/plot/NCAR-NEON")
+
+##Grab data for data products using Noble package
+dataMet <- lapply(listDpNum, function(x){
+  try(expr = neonUtilities::loadByProduct(site = Site, dpID = x, 
+                                          startdate = as.character(dateBgn), 
+                                          enddate = as.character(dateEnd), 
+                                          package = Pack, avg = TimeAgr, check.size = FALSE), 
+      silent = TRUE)
+})
+
+#Check if primary precipitation exists at the site, if not change to secondary precip
+varDp["PRECTmms_MDS"] <- ifelse(test = any(grepl(pattern = varDp["PRECTmms_MDS"], x = names(dataMet[["PRECTmms_MDS"]]))), "SECPRE_30min", "PRIPRE_30min")
+
+#Failsafe if using primary precip
+subVar["PRECTmms_MDS"] <- ifelse(test = varDp["PRECTmms_MDS"] == "SECPRE_30min", "secPrecipBulk", "priPrecipBulk")
+subVarQf["PRECTmms_MDS"] <- ifelse(test = varDp["PRECTmms_MDS"] == "SECPRE_30min", "secPrecipFinalQF", "priPrecipFinalQF")
+
+
+
+#Grab the actual data tables
+dataMetSub <- lapply(seq_along(varDp), function(x) {
+  tmp <- dataMet[[names(varDp[x])]][[grep(pattern = varDp[[x]], x = names(dataMet[[names(varDp[x])]]))]]
+  return(tmp)
+})
+
+#Name the output lists
+names(dataMetSub) <- names(varDp)
+
+#Remove unwanted measurement levels
+dataMetSub$Rg <- dataMetSub$Rg[dataMetSub$Rg$horizontalPosition == "000",]
+dataMetSub$FLDS_MDS <- dataMetSub$FLDS_MDS[dataMetSub$FLDS_MDS$horizontalPosition == "000",]
+dataMetSub$rH <- dataMetSub$rH[dataMetSub$rH$horizontalPosition == "003",]
+
+#time regularization of met data
+# dataMetSubRglr <- lapply(names(dataMetSub), function(x){
+#   timeRglrMet <- eddy4R.base::def.rglr(timeMeas = as.POSIXlt(dataMetSub[[x]]$startDateTime), 
+#                                        dataMeas = dataMetSub[[x]], 
+#                                        BgnRglr = as.POSIXlt(dateBgn), 
+#                                        EndRglr = as.POSIXlt(dateEnd), 
+#                                        TzRglr = "UTC", FreqRglr = 1/(60*30))
+#   
+#   return(timeRglrMet$dataRglr)
+# })#End lapply for time regularization of met data
+# 
+# #Add names to list of Dataframes of regularized data
+# names(dataMetSubRglr) <- names(varDp)
+
+#Grab just the Met data of interest for the forcing data
+dataDfMet <- lapply(seq_along(subVar), function(x){
+  #print(x)
+  #Grab the variables of interest
+  tmp <- dataMetSub[[names(subVar[x])]][,grep(pattern = paste0("^",subVar[x]), x = names(dataMetSub[[names(varDp[x])]]))] # use ^ to indicate the pattern starts with the name given
+  return(tmp)
+})
+
+#Give the ReddyProc names
+names(dataDfMet) <- names(varDp)
+
+#Calculate precip rate from bulk
+dataDfMet$PRECTmms_MDS <- dataDfMet$PRECTmms_MDS/1800 #1800 sec/0.5 hours
+
+#Calculate net radiation
+dataDfMet$radNet <-dataMetSub$Rg[["inSWMean"]] - dataMetSub$Rg[["outSWMean"]] + dataMetSub$Rg[["inLWMean"]] - dataMetSub$Rg[["outLWMean"]]
+
+#Add time
+dataDfMet$time <- dataMetSub$Pa_MDS$startDateTime
+
+#########################################################################################
+#QFQM
+#########################################################################################
+#Grab just the Met data of interest for the forcing data
+qfqmDfMet <- lapply(seq_along(subVar), function(x){
+  #print(x)
+  #Grab the variables of interest
+  tmp <- dataMetSub[[names(subVarQf[x])]][,grep(pattern = paste0("^",subVarQf[x]), x = names(dataMetSub[[names(varDp[x])]]))] # use ^ to indicate the pattern starts with the name given
+  return(tmp)
+})
+
+#Give the ReddyProc names
+names(qfqmDfMet) <- names(varDp)
+
+qfqmDfMet <- as.data.frame(qfqmDfMet)
+
+#Add time
+qfqmDfMet$time <- dataMetSub$Pa_MDS$startDateTime
+
+tmpRadQfqm <- select(dataMetSub$Rg,"inSWFinalQF", "outSWFinalQF", "outLWFinalQF","inLWFinalQF")
+
+
+#Add radNet
+qfqmDfMet$radNet <- ifelse(rowSums(tmpRadQfqm) > 0, 1, 0)
+
+qfqmDfMetLong <- gather(qfqmDfMet, "variable", "qfFinal", -time)
+
+#########################################################################################
+
+
+#Make a dataframe
+dataDfMet <- as.data.frame(dataDfMet)
+
+#Create long dataframe
+dataDfMetLong <- gather(dataDfMet, "variable", "value", PRECTmms_MDS, rH, FLDS_MDS, Rg, Pa_MDS, radNet)
+
+dataDfLong <- dplyr::left_join(dataDfMetLong, qfqmDfMetLong, by = c("time" = "time", "variable"="variable"))
+
+#########################################################################################
+#Plot
+#########################################################################################
+
+ggplot2::ggplot(dataDfLong, aes(x = time, y = value, colour = qfFinal == 1)) + geom_point(shape = 1) + 
+  scale_colour_manual(name = 'qfFinal = 1', values = setNames(c('red','black'), c(T, F))) +
+  facet_grid(~variable)
+
+
+
+#########################################################################################
+#Summary statistics
+#########################################################################################
+dataSummary <- dataDfLong %>% select(-time, -qfFinal) %>% group_by(variable) %>% summarise_all(list(mean = mean, median = median, max = max, min = min, sd = sd), na.rm = TRUE)
+
+qfqmSummary <-  dataDfLong %>% select(-time, -value) %>% group_by(variable) %>% summarise_all(list(sumQfFinal = sum, percentQfFinal = mean))
+
+summary <- dplyr::left_join(dataSummary, qfqmSummary, by = "variable")
