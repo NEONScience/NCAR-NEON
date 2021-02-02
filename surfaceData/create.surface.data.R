9fd133c01348699ad30c6d03db671f47b9eba9d8
ddurden <daviddurden27@gmail.com>
2021-02-02 17:32:59 +0000

    -creating a variable description

--- surfaceData/create.surface.data.R
+++ surfaceData/create.surface.data.R
@@ -45,10 +45,14 @@ lapply(packReq, function(x) {
 
 
 # Set directory - add for new users as needed
+inputs <- "~/eddy/data/CLM/surf/"
 if (file.exists('/Users/sweintraub/')) {
   inputs <- "/Users/sweintraub/Documents/GitHub/NCAR-NEON/surfaceData/"
   outputs <- paste0(inputs, "siteFiles/")
 }
+#if folder doesn't exist create it
+if(!dir.exists(inputs)) dir.create(inputs, recursive = TRUE)
+
 
 # Add info about megapit land cover (from TIS) and dominant plants in the tower (pheno)
 cover <- read.csv("https://s3.data.neonscience.org/neon-ncar/NEON/surf_files/inpMeta/tower.site.metadata.csv", 
@@ -99,8 +103,8 @@ mgp_all <- mgp_perbiogeo.1 %>%
       "collectDate",
       "laboratoryName",
       "labProjID", 
-      "publicationDate", 
-      "release"))
+      "publicationDate"))
+#,"release")) #Remove release until we update neonUtilities
 
 # Add pit-level metadata
 intersect(colnames(mgp_all), colnames(mgp_permegapit))
@@ -114,8 +118,8 @@ mgp_all.1 <- mgp_all %>%
     "pitID",
     "setDate",
     "collectDate",
-    "publicationDate",
-    "release"))
+    "publicationDate"))
+  #,"release"))#Remove release until we update neonUtilities
 
 # Add cover and dominant plants, arrange by pit and horizon, keep only vars needed by CLM
 intersect(colnames(mgp_all.1), colnames(cover))
@@ -159,8 +163,24 @@ for (i in 1:length(mgp_all.list)) {
   write.csv(mgp_all.list[[i]], file = paste0(outputs, names(mgp_all.list[i]), "_surfaceData.csv"), row.names = FALSE)
 }
 
+#List of variables output
+varList <- c("domainID", "siteID", "decimalLatitude","decimalLongitude","elevation","ecosystemType_Megapit","ecoregionWWF_Megapit","landCover_Megapit", "dominantPlants_Tower","horizonName", "biogeoTopDepth","biogeoBottomDepth", "carbonTot","nitrogenTot","phH2o", "coarseFrag2To5","coarseFrag5To20","coarseFrac2to20", "sandTotal", "siltTotal", "clayTotal", "bulkDensTopDepth", "bulkDensBottomDepth", "bulkDensExclCoarseFrag")
+
+#Remove duplicate variable descriptions
+varSub <- variables_00096[!duplicated(variables_00096$fieldName),]
+
+#Filter and subset variable description metadata
+varMeta <- varSub %>% filter(fieldName %in% varList) %>% select(fieldName, description, dataType, units)
+#Metadata for additional data streams
+varMetaAdd <- data.frame(fieldName = c("ecosystemType_Megapit","ecoregionWWF_Megapit","landCover_Megapit", "dominantPlants_Tower", "coarseFrac2to20"), description = c("The predominant ecosystem type found at the site","The ecoregion at the site","The landcover type found at the site","The most dominant plant species found at the site", "Coarse fragment (2-20 mm) content of the <20 mm size fraction of the biogeochemistry soil sample"), dataType = c("string","string","string","string","real"), units = c(NA,NA,NA,NA,"	gramsPerKilogram"))
+#Combine metadata to final dataframe
+varMetaSurf <- rbind(varMeta,varMetaAdd)
+
+#Sort dataframe alphabetically
+varMetaSurf <- varMetaSurf[order(varMetaSurf$fieldName),]
+
 # Also write the 'variables' file, for units
-write.csv(variables_00096, file = paste0(inputs, "variables.megapit.00096.csv"), row.names = FALSE)
+write.csv(varMetaSurf, file = paste0(inputs, "varMetaSurf.csv"), row.names = FALSE)
 
 
 
