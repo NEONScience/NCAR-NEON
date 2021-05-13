fe63887c2599416af65160d495b963bbf6a0244d
ddurden <daviddurden27@gmail.com>
2021-05-13 17:45:16 +0000

    - updating data download script.

--- flow/flow.dnld.neon.ncar.R
+++ flow/flow.dnld.neon.ncar.R
@@ -22,6 +22,8 @@
 # changelog and author contributions / copyrights
 #   David (2021-01-04)
 #     original creation
+#   David (2021-05-12)
+#     updating to grab all files and changing s3 bucket structure
 ##############################################################################################
 
 #site to download data for
@@ -33,12 +35,14 @@ TypeFile <- c("atm/cdeps", "surf_files", "eval_files")[1]
 #Adding version number to s3 path
 versData <- c("v1")
 
+#Base directory for downloading data
+DirDnldBase <- "~/eddy/data/CLM/dnld"
 
 #The version data for the FP standard conversion processing
 dnldDate <- paste0("v",format(Sys.time(), "%Y%m%d"))
 
 #Create download folder, create if it doesn't exist
-DirDnld <- paste0("~/eddy/data/CLM/dnld/",dnldDate,"/",site)
+DirDnld <- paste0(DirDnldBase,"/",dnldDate,"/",site)
 if(!dir.exists(DirDnld)) dir.create(DirDnld, recursive = TRUE)
 
 #Create data download string
@@ -47,12 +51,24 @@ DateEnd <- as.Date("2018-12-31")
 DateSeq <- seq.Date(from = DateBgn,to = DateEnd, by = "month")
 PrdWndwDnld <- strftime(DateSeq, format = "%Y-%m")
 
-
-#Create URL for data files
-urlDnld <- ifelse(TypeFile == "surf_files", paste0("https://s3.data.neonscience.org/neon-ncar/NEON/",TypeFile,"/",versData,"/",site,"_surfaceData.csv") ,paste0("https://s3.data.neonscience.org/neon-ncar/NEON/",TypeFile,"/",versData,"/",site,"/",PrdWndwDnld,".nc"))
-
-#Download filename (full path)
-fileDnld <-  ifelse(TypeFile == "surf_files", paste0(DirDnld,"/",site,"_",versData,"_surfaceData.csv"), paste0(DirDnld,"/",site,"_",versData,"_",PrdWndwDnld,".nc"))
+#logical statement about type of files
+if(TypeFile == "surf_files"){
+  urlDnld <- paste0("https://s3.data.neonscience.org/neon-ncar/NEON/",TypeFile,"/",versData,"/",site,"_surfaceData.csv")
+  
+  #Download filename (full path)
+  fileDnld <- paste0(DirDnld,"/",site,"_",versData,"_surfaceData.csv")
+} else
+  {
+  #Create URL for data files
+  urlDnld <- paste0("https://s3.data.neonscience.org/neon-ncar/NEON/",TypeFile,"/",versData,"/",site,"/",PrdWndwDnld,".nc")
+  
+  #Removing extra nesting used by CLM for atm files
+  TypeFileDnld <- unlist(strsplit(TypeFile, "/|_"))[1]
+  
+  #Download filename (full path)
+  fileDnld <-  paste0(DirDnld,"/",site,"_",TypeFileDnld,"_",versData,"_",PrdWndwDnld,".nc") 
+  
+} #End of else for logical statement about type of files
 
 #Download files
 sapply(seq_along(urlDnld), function(x){
