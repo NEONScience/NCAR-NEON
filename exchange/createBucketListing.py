5bb31ff3407cc4ff82989a6f27ee08ecf66a5e87
Dawn Lenz <dlenz@battelleecology.org>
2022-04-07 16:47:49 -0600

    move bucket listing to GCS

--- exchange/createBucketListing.py
+++ exchange/createBucketListing.py
@@ -1,6 +1,3 @@
-import os
-from boto3 import resource
-import json
 from google.cloud import storage
 import base64
 import binascii
@@ -8,7 +5,6 @@ import binascii
 
 def createBucketListing():
 
-    # During migration to GCS (until sync is disabled), list objects from GCS and upload listing.csv to ECS
     gcsPrefix = "https://storage.neonscience.org/"
     gcsBucketName = "neon-ncar"
     storage_client = storage.Client()
@@ -18,22 +14,9 @@ def createBucketListing():
         checksum = binascii.hexlify(base64.urlsafe_b64decode(blob.md5_hash)).decode()
         listing += gcsPrefix + gcsBucketName + "/" + blob.name + ',' + blob.updated.strftime("%Y-%m-%d %H:%M:%S") + ',' + checksum + '\n' 
 
-    s3 = getTargetS3("neon-ncar-writer")
-    outputObject = s3.Object("neon-ncar", "listing.csv")
-    outputObject.put(Body=listing)
-
-
-def getTargetS3(targetRemote):
-    # Parse target access from config
-    s3configfile = os.getenv('S3_NC_CONFIG')
-    with open(s3configfile) as f:
-        s3config = json.load(f)
-    s3uri = "s3://" + targetRemote
-    targetUrl = s3config['hosts'][s3uri]['url']
-    accessKey = s3config['hosts'][s3uri]['credentials']['accessKey']
-    secretKey = s3config['hosts'][s3uri]['credentials']['secretKey']
-    s3 = resource('s3', aws_access_key_id=accessKey, aws_secret_access_key=secretKey, endpoint_url=targetUrl)
-    return s3
+    gcsBucket = storage_client.bucket(gcsBucketName)
+    listingBlob = gcsBucket.blob("listing.csv")
+    listingBlob.upload_from_string(listing)
 
 
 if __name__ == '__main__':
